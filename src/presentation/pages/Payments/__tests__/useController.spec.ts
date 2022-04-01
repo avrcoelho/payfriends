71381a02521512051b28ec0776e68c4d8e7c48e0
avrcoelho <andrevrcoelho@hotmail.com>
2022-04-01 13:00:40 -0300

    feat: page payments hook controller

---
+++ src/presentation/pages/Payments/__tests__/useController.spec.ts
@@ -0,0 +1,104 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import { useController } from '../useController';
+
+const mockNotificationError = jest.fn();
+jest.mock('react-hook-notification', () => ({
+  useNotification: () => ({
+    error: mockNotificationError,
+  }),
+}));
+
+const user = {
+  id: '7',
+  name: 'John Doe',
+  nickname: 'johndoe',
+  email: 'john@doe.com',
+  avatar: 'https://github.com/avrcoelho.png',
+};
+
+describe('Payments page hook controller', () => {
+  const props = {
+    getUser: jest.fn(),
+    getPayments: jest.fn(),
+  };
+
+  it('should be able to dispatch notification when has error on get user data', async () => {
+    props.getUser.mockImplementationOnce(() => ({
+      execute: jest.fn().mockRejectedValueOnce(''),
+    }));
+    renderHook(() => useController(props));
+
+    await act(() => Promise.resolve());
+
+    expect(mockNotificationError).toBeCalled();
+  });
+
+  it('should be able to dispatch notification when has error on get payments', async () => {
+    props.getUser.mockImplementationOnce(() => ({
+      execute: jest.fn().mockResolvedValueOnce(user),
+    }));
+    props.getPayments.mockImplementationOnce(() => ({
+      execute: jest.fn().mockRejectedValueOnce(''),
+    }));
+    renderHook(() => useController(props));
+
+    await act(() => Promise.resolve());
+
+    expect(mockNotificationError).toBeCalled();
+  });
+
+  it('should be able to return payments', async () => {
+    props.getUser.mockImplementationOnce(() => ({
+      execute: jest.fn().mockResolvedValueOnce(user),
+    }));
+    props.getPayments.mockImplementationOnce(() => ({
+      execute: jest.fn().mockResolvedValueOnce([]),
+    }));
+    const { result, waitFor } = renderHook(() => useController(props));
+
+    await act(() => Promise.resolve());
+
+    await waitFor(() => {
+      expect(result.current.payments).toEqual([]);
+    });
+  });
+
+  it('should be able to update page', async () => {
+    props.getUser.mockImplementationOnce(() => ({
+      execute: jest.fn().mockResolvedValueOnce(user),
+    }));
+    props.getPayments.mockImplementationOnce(() => ({
+      execute: jest.fn().mockResolvedValueOnce([]),
+    }));
+    const { result, waitFor } = renderHook(() => useController(props));
+
+    await act(() => Promise.resolve());
+    act(() => {
+      result.current.onUpdatePage(2);
+    });
+
+    await waitFor(() => {
+      expect(result.current.page).toBe(2);
+    });
+  });
+
+  it('should be able to update limit', async () => {
+    props.getUser.mockImplementationOnce(() => ({
+      execute: jest.fn().mockResolvedValueOnce(user),
+    }));
+    props.getPayments.mockImplementationOnce(() => ({
+      execute: jest.fn().mockResolvedValueOnce([]),
+    }));
+    const { result, waitFor } = renderHook(() => useController(props));
+
+    await act(() => Promise.resolve());
+    act(() => {
+      result.current.onUpdateLimit(2);
+    });
+
+    await waitFor(() => {
+      expect(result.current.limit).toBe(2);
+    });
+  });
+});
---
+++ src/presentation/pages/Payments/useController.ts
@@ -0,0 +1,103 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useNotification } from 'react-hook-notification';
+
+import { User } from '@/entities/User';
+import { Payment } from '@/entities/Payment';
+import { useQuery } from '@/presentation/hooks/useQuery';
+import { GetUser } from '@/useCases/GetUser';
+import { GetPayments } from '@/useCases/GetPayments';
+import { useStore } from '@/presentation/store/useStore';
+
+type UseControllerHookProps = {
+  getUser: () => GetUser;
+  getPayments: () => GetPayments;
+};
+
+type UseControllerHook = (props: UseControllerHookProps) => {
+  isLoadingUser: boolean;
+  isLoadingPayments: boolean;
+  user: User | undefined;
+  payments: Payment[] | undefined;
+  page: number;
+  limit: number;
+  onUpdateLimit(value: number): void;
+  onUpdatePage(value: number): void;
+};
+
+export const useController: UseControllerHook = ({ getUser, getPayments }) => {
+  const [limit, setLimit] = useState(5);
+  const [page, setPage] = useState(1);
+
+  const notification = useNotification({ position: 'top-left' });
+  const userId = useStore(state => state.userId);
+  const {
+    isError: isErrorUser,
+    isSuccess: isSuccessUser,
+    isLoading: isLoadingUser,
+    data: user,
+  } = useQuery(() => getUser().execute(String(userId)));
+  const {
+    isError: isErrorPayments,
+    isLoading: isLoadingPayments,
+    data: payments,
+    refetch,
+  } = useQuery(
+    () => getPayments().execute({ limit, page, userId: String(userId) }),
+    {
+      manualFetch: true,
+    },
+  );
+
+  const dispatchErrorNotification = useCallback(
+    (message: string) => {
+      notification.error({
+        text: message,
+      });
+    },
+    [notification],
+  );
+
+  useEffect(() => {
+    if (isErrorUser) {
+      dispatchErrorNotification(
+        'Um erro ocorreu ao obter os dados do usuário. Tente novamente!',
+      );
+    }
+    if (isErrorPayments) {
+      dispatchErrorNotification(
+        'Um erro ocorreu ao obter os pagamentos. Tente novamente!',
+      );
+    }
+  }, [dispatchErrorNotification, isErrorUser, isErrorPayments]);
+
+  useEffect(() => {
+    if (isSuccessUser) {
+      refetch();
+    }
+  }, [isSuccessUser, refetch]);
+
+  useEffect(() => {
+    if (!!payments) {
+      refetch();
+    }
+  }, [payments, refetch, limit, page]);
+
+  const onUpdatePage = useCallback((newPage: number) => {
+    setPage(newPage);
+  }, []);
+
+  const onUpdateLimit = useCallback((newLimit: number) => {
+    setLimit(newLimit);
+  }, []);
+
+  return {
+    isLoadingUser,
+    user,
+    isLoadingPayments,
+    limit,
+    page,
+    payments,
+    onUpdateLimit,
+    onUpdatePage,
+  };
+};
