83136d17ac72580930c6c9969aefd82a6408eeae
avrcoelho <andrevrcoelho@hotmail.com>
2022-04-02 00:53:32 -0300

    refactor: store

--- src/presentation/store/__tests__/useStore.spec.ts
+++ src/presentation/store/__tests__/useStore.spec.ts
@@ -1,5 +1,6 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 
+import { Payment } from '@/entities/Payment';
 import { useStore } from '../useStore';
 
 describe('useStore', () => {
@@ -24,5 +25,75 @@ describe('useStore', () => {
     });
 
     expect(result.current.userId).toBeNull();
+  });
+
+  const payments = [
+    {
+      user: {
+        id: '7',
+      },
+      id: '7',
+      value: 700,
+      timestamp: Date.now(),
+      status: true,
+    },
+    {
+      user: {
+        id: '7',
+      },
+      id: '0',
+      value: 700,
+      timestamp: Date.now(),
+      status: true,
+    },
+  ] as Payment[];
+
+  it('should be able to set paymnets', () => {
+    const { result } = renderHook(() => useStore());
+
+    act(() => {
+      result.current.onSetPayments(payments);
+    });
+
+    expect(result.current.payments).toEqual(payments);
+  });
+
+  it('should be able to update paymnet', () => {
+    const { result } = renderHook(() => useStore());
+
+    act(() => {
+      result.current.onSetPayments(payments);
+    });
+    act(() => {
+      result.current.onUpdatePayment({ ...payments[0], value: 10 });
+    });
+
+    expect(result.current.payments[0].value).toBe(10);
+  });
+
+  it('should be able to delete paymnet', () => {
+    const { result } = renderHook(() => useStore());
+
+    act(() => {
+      result.current.onSetPayments(payments);
+    });
+    act(() => {
+      result.current.onDeletePayment('7');
+    });
+
+    expect(result.current.payments).toHaveLength(1);
+  });
+
+  it('should be able to update status paymnet', () => {
+    const { result } = renderHook(() => useStore());
+
+    act(() => {
+      result.current.onSetPayments(payments);
+    });
+    act(() => {
+      result.current.onUpdatePaymentStatus({ id: '7', status: false });
+    });
+
+    expect(result.current.payments[0].status).toBeFalsy();
   });
 });
--- src/presentation/store/useStore.ts
+++ src/presentation/store/useStore.ts
@@ -1,17 +1,55 @@
 import create from 'zustand';
 
-interface StoreState {
+import { Payment } from '@/entities/Payment';
+
+type PaymentStatusProps = {
+  id: string;
+  status: boolean;
+};
+
+type StoreState = {
   userId: string | number | null;
+  payments: Payment[];
   onSaveUserId(userId: string): void;
+  onSetPayments(payments: Payment[]): void;
+  onUpdatePayment(payment: Payment): void;
+  onDeletePayment(paymentId: string): void;
+  onUpdatePaymentStatus(props: PaymentStatusProps): void;
   onDeleteUserId(): void;
-}
+};
 
 export const useStore = create<StoreState>(set => ({
   userId: '',
+  payments: [],
   onSaveUserId: userId => {
     set(state => ({ ...state, userId }));
   },
   onDeleteUserId: () => {
     set(state => ({ ...state, userId: null }));
+  },
+  onSetPayments: payments => {
+    set(state => ({ ...state, payments }));
+  },
+  onUpdatePayment: payment => {
+    set(state => ({
+      ...state,
+      payments: state.payments.map(pmt =>
+        pmt.id === payment.id ? payment : pmt,
+      ),
+    }));
+  },
+  onUpdatePaymentStatus: ({ id, status }) => {
+    set(state => ({
+      ...state,
+      payments: state.payments.map(pmt =>
+        pmt.id === id ? { ...pmt, status } : pmt,
+      ),
+    }));
+  },
+  onDeletePayment: paymentId => {
+    set(state => ({
+      ...state,
+      payments: state.payments.filter(payment => payment.id !== paymentId),
+    }));
   },
 }));
