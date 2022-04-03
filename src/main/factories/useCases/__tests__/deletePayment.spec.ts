b41de0dbc31566aa2640d6ac3a3d75ccb28a2ead
avrcoelho <andrevrcoelho@hotmail.com>
2022-04-02 21:14:00 -0300

    fix: test

--- src/main/factories/useCases/__tests__/deletePayment.spec.ts
+++ src/main/factories/useCases/__tests__/deletePayment.spec.ts
@@ -2,6 +2,6 @@ import { makeDeletePayment } from '../deletePayment';
 
 describe('make delete payment', () => {
   it('should be able to exists execute method', () => {
-    expect(makeDeletePayment()).toHaveProperty('execute');
+    expect(typeof makeDeletePayment).toBe('function');
   });
 });
