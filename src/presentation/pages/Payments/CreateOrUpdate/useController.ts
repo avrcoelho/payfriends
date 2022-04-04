a50242cc27d0370f9383d5f6d54ee0d168b66289
avrcoelho <andrevrcoelho@hotmail.com>
2022-04-04 20:10:41 -0300

    done

--- src/presentation/pages/Payments/CreateOrUpdate/useController.ts
+++ src/presentation/pages/Payments/CreateOrUpdate/useController.ts
@@ -64,7 +64,7 @@ export const useController: UseControllerHook = ({
       value: paymentToUpdate?.value,
       date: paymentToUpdate?.date?.replace(
         /(\d{4})-(\d{2})-(\d{2})/,
-        '$3-$2-$1',
+        '$3/$2/$1',
       ),
       title: paymentToUpdate?.title,
     },
